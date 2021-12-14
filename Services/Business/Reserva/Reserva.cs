54021c5e838a330284f05b017e92e8922614a022
carlosas <carlosabs@algartech.com>
2021-12-14 18:00:49 -0500

    Actualización librería

---
+++ Services/Business/Reserva/Reserva.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Reserva
+{
+    public class Reserva
+    {
+        public int GetReserva() {
+            return 1;
+        }
+    }
+}
